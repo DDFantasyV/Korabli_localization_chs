3bd6bbc12f3570aa493beb140d7ef506e0bc50bd
MFunction96 <mfunction96@outlook.com>
2024-09-24 14:01:39 +0800

    迁移Gitee镜像工作流至Azure DevOps Pipeline

--- .azurepipelines/Replica.ps1
+++ .azurepipelines/Replica.ps1
@@ -3,7 +3,11 @@ param(
     [string] $AccessToken
 )
 
-$githubReleaseApiUrl = "https://api.github.com/repos/MFunction96/KorabliChsMod/releases"
+git checkout main
+git push git@gitee.com:repad/Korabli_localization_chs.git --force
+git push git@gitee.com:repad/Korabli_localization_chs.git --tags
+
+$githubReleaseApiUrl = "https://api.github.com/repos/DDFantasyV/Korabli_localization_chs/releases"
 $githubReleaseHeaders = @{
 	"Accept" = "application/vnd.github.v3+json"
 	"X-GitHub-Api-Version" = "2022-11-28"
@@ -12,7 +16,7 @@ $githubReleaseHeaders = @{
 $githubResponse = Invoke-RestMethod -Uri $githubReleaseApiUrl -Headers $githubReleaseHeaders -Method Get
 $latestGithubRelease = $githubResponse[0]
 
-$giteeReleaseApiUrl = "https://gitee.com/api/v5/repos/MFunction96/KorabliChsMod/releases"
+$giteeReleaseApiUrl = "https://gitee.com/api/v5/repos/repad/Korabli_localization_chs/releases"
 $giteeReleaseHeaders = @{
 	"Content-Type" = "application/json;charset=UTF-8"
 }
