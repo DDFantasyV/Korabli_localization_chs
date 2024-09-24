769bb6fad97a33b8399d5a81eb45b105def4383d
MFunction96 <mfunction96@outlook.com>
2024-09-24 14:17:48 +0800

    修正镜像逻辑并添加临时文件存储管理

--- .azurepipelines/Replica.ps1
+++ .azurepipelines/Replica.ps1
@@ -1,33 +1,54 @@
 param(
     [Parameter(Mandatory = $true)]
-    [string] $AccessToken
+    [string] $AccessToken,
+	[Parameter(Mandatory = $false)]
+	[string] $Branch = 'main'
 )
 
-git checkout main
-git push git@gitee.com:repad/Korabli_localization_chs.git --force
-git push git@gitee.com:repad/Korabli_localization_chs.git --tags
-
-$githubReleaseApiUrl = "https://api.github.com/repos/DDFantasyV/Korabli_localization_chs/releases"
-$githubReleaseHeaders = @{
-	"Accept" = "application/vnd.github.v3+json"
-	"X-GitHub-Api-Version" = "2022-11-28"
+if ([string]::IsNullOrEmpty($env:TMP)) {
+	$env:TMP = '/tmp'
 }
 
-$githubResponse = Invoke-RestMethod -Uri $githubReleaseApiUrl -Headers $githubReleaseHeaders -Method Get
-$latestGithubRelease = $githubResponse[0]
-
-$giteeReleaseApiUrl = "https://gitee.com/api/v5/repos/repad/Korabli_localization_chs/releases"
-$giteeReleaseHeaders = @{
-	"Content-Type" = "application/json;charset=UTF-8"
+$tmpPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), [guid]::NewGuid().ToString())
+if ([System.IO.Directory]::Exists($tmpPath)) {
+	Remove-Item -Path $tmpPath -Force -Recurse
 }
-$giteeReleaseBody = @{
-	"access_token" = ${AcccessToken}
-	"tag_name" = $latestGithubRelease.tag_name
-	"name" = $latestGithubRelease.name
-	"body" = $latestGithubRelease.body
-	"prerelease" = $latestGithubRelease.prerelease
-	"target_commitish" = $latestGithubRelease.target_commitish
-} | ConvertTo-Json
 
-$giteeResponse = Invoke-RestMethod -Uri $giteeReleaseApiUrl -Headers $giteeReleaseHeaders -Method Post -Body $giteeReleaseBody
-$giteeResponse | Out-Null
+[System.IO.Directory]::CreateDirectory($tmpPath)
+try {
+	Set-Location $tmpPath
+	git clone git@github.com:DDFantasyV/Korabli_localization_chs.git --branch $Branch
+	Set-Location Korabli_localization_chs
+	git push git@gitee.com:repad/Korabli_localization_chs.git --force
+	git push git@gitee.com:repad/Korabli_localization_chs.git --tags
+	$githubReleaseApiUrl = "https://api.github.com/repos/DDFantasyV/Korabli_localization_chs/releases"
+	$githubReleaseHeaders = @{
+		"Accept" = "application/vnd.github.v3+json"
+		"X-GitHub-Api-Version" = "2022-11-28"
+	}
+
+	$githubResponse = Invoke-RestMethod -Uri $githubReleaseApiUrl -Headers $githubReleaseHeaders -Method Get
+	$latestGithubRelease = $githubResponse[0]
+
+	$giteeReleaseApiUrl = "https://gitee.com/api/v5/repos/repad/Korabli_localization_chs/releases"
+	$giteeReleaseHeaders = @{
+		"Content-Type" = "application/json;charset=UTF-8"
+	}
+	$giteeReleaseBody = @{
+		"access_token" = ${AcccessToken}
+		"tag_name" = $latestGithubRelease.tag_name
+		"name" = $latestGithubRelease.name
+		"body" = $latestGithubRelease.body
+		"prerelease" = $latestGithubRelease.prerelease
+		"target_commitish" = $latestGithubRelease.target_commitish
+	} | ConvertTo-Json
+
+	$giteeResponse = Invoke-RestMethod -Uri $giteeReleaseApiUrl -Headers $giteeReleaseHeaders -Method Post -Body $giteeReleaseBody
+	$giteeResponse | Out-Null
+}
+catch {
+	throw $_
+}
+finally {
+	Remove-Item -Path $tmpPath -Force -Recurse
+}
