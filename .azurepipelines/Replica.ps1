815a3facc3c6e5486ae8a425447b0ed7ab1603f8
MFunction96 <mfunction96@outlook.com>
2025-01-12 20:43:38 +0800

    添加Cloudflare CDN镜像脚本

---
+++ .azurepipelines/Replica.ps1
@@ -0,0 +1,11 @@
+param(
+	[bool]$isTest = $false
+)
+
+$modName = "Korabli_localization_chs"
+if ($isTest) {
+	$modName = $modName + "_test"
+}
+
+Compress-Archive -Path _DDFantasyV_Korabli_localization_chs -DestinationPath Korabli_localization_chs.zip -CompressionLevel Optimal -Force
+aws s3api put-object --bucket warshipmod --key "mods/chs/$modName.zip" --body Korabli_localization_chs.zip
