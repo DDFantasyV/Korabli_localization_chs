37f4369e7af24277d44dc4af3f4f52e0a1e2cd8a
MFunction96 <mfunction96@outlook.com>
2025-05-23 16:50:11 +0800

    更新镜像逻辑

--- .azurepipelines/Replica.ps1
+++ .azurepipelines/Replica.ps1
@@ -1,5 +1,5 @@
 param(
-	[bool]$isTest = $false
+	[switch]$isTest = $false
 )
 
 $modName = "Korabli_localization_chs"
@@ -7,5 +7,12 @@ if ($isTest) {
 	$modName = $modName + "_test"
 }
 
-Compress-Archive -Path _DDFantasyV_Korabli_localization_chs -DestinationPath Korabli_localization_chs.zip -CompressionLevel Optimal -Force
+$compressOptions = @{
+	Path = "_DDFantasyV_Korabli_localization_chs/locale_config.xml", "_DDFantasyV_Korabli_localization_chs/thanks.md", "_DDFantasyV_Korabli_localization_chs/LICENSE", "_DDFantasyV_Korabli_localization_chs/change.log", "_DDFantasyV_Korabli_localization_chs/texts/"
+	CompressionLevel = "Optimal"
+	DestinationPath = "Korabli_localization_chs.zip"
+	Force = $true
+}
+
+Compress-Archive @compressOptions
 aws s3api put-object --bucket warshipmod --key "mods/chs/$modName.zip" --body Korabli_localization_chs.zip
